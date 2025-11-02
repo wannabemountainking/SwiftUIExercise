4aec4fde53add8f9e2ad8d44e1bf79c7a36737e9
wannabemountainking <kdy0096@gmail.com>
2025-11-02 16:48:21 +0900

    Initial Commit

---
+++ SwiftUIExercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIExercise
+//
+//  Created by YoonieMac on 11/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIExercise/SwiftUIExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExerciseApp.swift
+//  SwiftUIExercise
+//
+//  Created by YoonieMac on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
