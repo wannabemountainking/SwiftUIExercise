b6854f93f7922a764fd22a6fd1b2726cf4d8487f
wannabemountainking <87057243+wannabemountainking@users.noreply.github.com>
2025-11-08 22:57:57 +0900

    BindingPresenting

---
+++ SwiftUIExercise/SheetNavigationView/BindingPresenting/MemoSystem.swift
@@ -0,0 +1,18 @@
+//
+//  MemoSystem.swift
+//  SwiftUIExercise
+//
+//  Created by YoonieMac on 11/10/25.
+//
+
+import SwiftUI
+
+struct MemoSystem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MemoSystem()
+}
