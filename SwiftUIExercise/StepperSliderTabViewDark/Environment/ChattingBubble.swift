2570831097e6ffb12953e2ac2c0f65dfa49c7fdf
wannabemountainking <87057243+wannabemountainking@users.noreply.github.com>
2025-11-23 15:02:39 +0900

    TabView

---
+++ SwiftUIExercise/StepperSliderTabViewDark/Environment/ChattingBubble.swift
@@ -0,0 +1,18 @@
+//
+//  ChattingBubble.swift
+//  SwiftUIExercise
+//
+//  Created by yoonie on 11/23/25.
+//
+
+import SwiftUI
+
+struct ChattingBubble: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChattingBubble()
+}
